ddebbf19af8d4fdde443ad0d2c1524e1b1237af8
Stephen Krewson <stephen.krewson@gmail.com>
2017-11-16 21:49:50 -0500

    ok now i have a way of binning them

---
+++ stats.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from collections import defaultdict
+import fnmatch
+import os
+import sys
+
+
+IMG_DIR = '/mnt/c/Users/stephen-krewson/Dropbox/parley-train'
+
+year_hist = defaultdict(lambda: 0)
+
+
+for d in os.listdir(IMG_DIR):
+
+	#print(d, end=': ')
+
+	year_size = len(os.listdir(os.path.join(IMG_DIR, d)))
+
+	#print(year_size)
+	
+	# round DOWN to the half-decade
+	year_hist[int(d) - int(d) % 5] += year_size
+
+	
+for k,v in sorted(year_hist.items()):
+	print(k,'-',k+5,':',v)
+
