34e4bb551396293bce5bcab63e3dafcfdc2c229f
Stephen Krewson <stephen.krewson@gmail.com>
2017-11-19 14:40:42 -0500

    ok cleaned up

--- stats.py
+++ stats.py
@@ -9,7 +9,7 @@ import sys
 IMG_DIR = '/mnt/c/Users/stephen-krewson/Dropbox/parley-train'
 
 year_hist = defaultdict(lambda: 0)
-
+total = 0
 
 for d in os.listdir(IMG_DIR):
 
@@ -21,8 +21,11 @@ for d in os.listdir(IMG_DIR):
 	
 	# round DOWN to the half-decade
 	year_hist[int(d) - int(d) % 5] += year_size
-
+	total += year_size
 	
 for k,v in sorted(year_hist.items()):
 	print(k,'-',k+4,':',v)
 
+
+
+print("Total images:", total)
