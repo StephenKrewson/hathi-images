1fd48d4eaaef390fbc7234992d65a9d13f932998
Stephen Krewson <stephen.krewson@gmail.com>
2019-07-31 12:12:33 -0400

    500k for only google ids

--- ids_from_range.py
+++ ids_from_range.py
@@ -6,9 +6,8 @@ import pandas as pd
 import sys
 
 
-# Usually around 1 GB in size; just DL and store locally
-# updates are not super important for this project
-HATHIFILE = "hathifiles/hathi_full_20190601.txt.gz"
+# field names corrected July 1, 2019--using July data
+HATHIFILE = "hathifiles/hathi_full_20190701.txt.gz"
 HATHICOLS = "hathifiles/hathi_field_list.txt"
 
 
@@ -19,64 +18,61 @@ def ids_from_hathifile(htfile, htcols, start, end):
     Currently (2019) the only way to programmatically search HT
     """
 
-    # June 2018 is 4GB! col 0=volume_id, col 16=pub_date
-    # https://stackoverflow.com/questions/25962114/how-to-read-a-6-gb-csv-file-with-pandas
+    # See: https://stackoverflow.com/questions/25962114/how-to-read-a-6-gb-csv-file-with-pandas
     # https://stackoverflow.com/questions/13651117/how-can-i-filter-lines-on-load-in-pandas-read-csv-function
 
-    # get tab-separated column names; there are 26
-    # UPDATE! these names are wrong!
     with open(htcols, "r") as fp:
         col_names = fp.readline().strip('\n').split('\t')
-        num_cols = len(col_names) 
+        num_cols = len(col_names)
 
     # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
     iter_csv = pd.read_csv(
         htfile, 
         sep='\t', 
-        header=None, 
+        header=None,
+        names=col_names,
         engine='c', 
-        dtype={'htid': 'str', 16: 'object'},
+        dtype={'htid': 'str', 'rights_date_used': 'object'},
         iterator=True,
         chunksize=10000,
         error_bad_lines=False)
 
     df = pd.DataFrame()
+    for i, chunk in enumerate(iter_csv):
 
-    # N.B. not all pub dates are valid numbers
-    for chunk in iter_csv:
+        # 17m / 10k ~= 1700 chunks (30-60 minutes on my laptop)
+        print("Processing chunk", i)
 
-        # force each chunk to have a numeric pub date
-        chunk[16] = chunk[16].apply(pd.to_numeric, errors='coerce')
+        # force each chunk to have an integer publication date
+        # N.B unknown pub dates are marked with '9999'
+        chunk['rights_date_used'] = chunk['rights_date_used'].apply(pd.to_numeric, errors='coerce', downcast='integer')
         
-        # concatenate valid rows, idx doesn't matter
-        df = pd.concat(
-            [df, chunk[(chunk[16] >= start) & (chunk[16] <= end)]],
-            ignore_index=True)
+        # the conditions:
+        # 1) date range within [START,END]
+        # 2) access_profile_code == 'google' (ignore case)
+        conditions = (chunk['rights_date_used'] >= start) & (chunk['rights_date_used'] <= end) & (chunk['access_profile_code'] == 'google') 
 
-    print("Size:", df.shape)
+        # concatenate valid rows, idx doesn't matter
+        df = pd.concat([df, chunk[conditions]], ignore_index=True)
+
+    print("Size of filtered table:", df.shape)
     return df
 
 
-# TODO: use argparse
+# Using conda environment 'htrc'
 if __name__ == '__main__':
 
     if len(sys.argv) != 4:
-        sys.exit("USAGE: python -m src.ids_from_range <START_DATE> <END_DATE> <OUT.csv>")
+        sys.exit("USAGE: python ids_from_range.py <START_DATE> <END_DATE> <OUT.csv>")
 
     # positional args
     start = int(sys.argv[1])
     end = int(sys.argv[2])
     out = sys.argv[3]
 
-    print("Creating {} for HathiTrust volumes from {} TO {}.".format(out, start, end))
+    print("Creating {} for Google-digitized HathiTrust volumes from {} TO {}.".format(out, start, end))
 
-    # get valid rows and save
+    # get valid rows and save (preserve HathiFile delimiter)
     volumes = ids_from_hathifile(HATHIFILE, HATHICOLS, start, end)
+    volumes.to_csv(out, sep='\t', index=False, compression='gzip')
     
-
-    # TODO: keep separator as TAB, drop index
-    # then make changes in sample_from_ids
-    # tell Ryan about differences
-
-    # will this work from Windows shell?? better to compress by hand
-    volumes.to_csv(out, index=False, compression='gz')
