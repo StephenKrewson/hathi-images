0c82ce076a459584dec364ef44cb5bcc570b3145
Stephen Krewson <stephen.krewson@gmail.com>
2019-08-24 14:54:52 -0400

    need to get test csv file where column 0 is to jp2s

--- filter_image_csv.py
+++ filter_image_csv.py
@@ -2,28 +2,41 @@
 #
 # filter_image_csv.py
 #
-# Takes a CSV with a column 
+# Input: a CSV file where an image resource is the *first* column
+# Output: the same CSV file but with all the rows removed that do not
+# have `inline_image`as the top prediction for the image
 #
 # Run python filter_image_csv.py [--help] for usage 
 
 
 import argparse
-from fastai.vision import *
-import pandas as pd
+import csv
+#from fastai.vision import *
+import os
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Using a 12-class CNN model, runs inference on a CSV list of image assets and writes out a list of only those images for which the class inline_image was the highest prediction.')
-
-    parser.add_argument('--input', required=True, action='store_true', help='Path to input CSV file.')
-
-    parser.add_argument('--fields', required=True, action='store_true', help='Path to .txt file listing column names for input CSV.')
-
-    parser.add_argument('--output', required=True, action='store_true', help='Path to output directory for filtered CSV file. Must have write permissions.')
-    
+    parser.add_argument('--input', required=True, type=argparse.FileType('r'), help='Path to input CSV file.')
+    parser.add_argument('--model', required=True, type=argparse.FileType('r'), help='Path to pickled CNN model.')
+    parser.add_argument('--output', required=True, type=dir_path, help='Path to output directory for filtered CSV file. Must have write permissions.')
     return parser.parse_args()
 
 
-def main():
-    args = parse_args()
-    print(args)
+def dir_path(path):
+    if os.path.isdir(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"dir:{path} is not a valid path.")
+
+
+### Main script ###
+args = parse_args()
+
+# open the model (should be .pkl file)
+
+# argparse has already opened the CSV chunk
+for row in csv.reader(args.input):
+
+    # now do inference
+    print(row[0])
