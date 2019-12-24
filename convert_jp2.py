a72af3de2bea0a5aee4f2e00249cfe7365991f8e
Stephen Krewson <stephen.krewson@gmail.com>
2019-12-24 09:56:14 -0500

    collect jupyter notebooks

--- convert_jp2.py
+++ convert_jp2.py
@@ -1,5 +1,21 @@
+import glob
 import imageio
+import os
+import sys
 
 
-# use conda
-# cf with code in utils.py
+if __name__ == "__main__":
+    
+    if len(sys.argv) != 2:
+        sys.exit("USAGE: python -m scripts.convert_jp2 <DIR>")
+
+    # get all jpegs in a specified folder
+    directory_path = os.path.join(sys.argv[1], "*.jp2")
+    path_list = glob.glob(directory_path)
+    
+
+    # still having issues with JP2 conversion...
+    # just use direct JPEG download from PH lesson
+    for path in path_list:
+        img = imageio.imread(path)
+        imageio.imwrite(path, img, format="jpg")
