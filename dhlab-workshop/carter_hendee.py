d0ceb5cafdbc7b24daf16f5b41a369ef7ab64fee
Stephen Krewson <stephen.krewson@gmail.com>
2021-04-10 09:13:56 -0400

    backup computer to get the dhlab stuff

--- dhlab-workshop/carter_hendee.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function
-import json
-
-
-# hathitrust ids are within the "gather" field
-with open("carter-hendee-metadata.json", "r") as fp:
-    data = json.load(fp)
-htids = [item['htitem_id'] for item in data['gathers']]
-
--- dhlab-workshop/config.py
+++
@@ -1,4 +0,0 @@
-ht_keys = {
-	'access_key': '8836421f81',
-	'secret_key': '6c5e4d390d64a365cd22fda39778'
-}
